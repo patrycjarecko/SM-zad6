0ed3662feccf440a929ebb00a51692ffe9b446ac
patrycjarecko <Pprki2019>
2021-11-04 21:28:12 +0100

    initial commit

---
+++ app/src/main/java/com/example/zad6/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.zad6;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import android.os.Bundle;
+
+import java.util.UUID;
+
+public class MainActivity extends SingleFragmentActivity {
+
+
+    @Override
+    protected Fragment createFragment() {
+        UUID taskId = (UUID) getIntent().getSerializableExtra(TaskListFragment.KEY_EXTRA_TASK_ID);
+        return TaskFragment.newInstance(taskId);
+    }
+}
